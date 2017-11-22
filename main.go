96efecf0f2b5a070a2dd1a2c9675c297852d8814
Victor <victor@localhost.localdomain>
2017-11-22 13:35:43 -0200

    Adicionada a função de cadastrar novos clientes

--- main.go
+++ main.go
@@ -27,16 +27,14 @@ type Clientes struct {
 //Handler para a pagina inicial:
 //Conversa com o banco de dados e com a página HTML
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-  url := "localhost"
   //inicia uma sessão no mongodb e trato um possivel erro
+  url := "localhost"
   sessao, err := mgo.Dial(url)
   if err != nil {
     log.Fatal(err)
   }
   //Abre a coleção cliente
   colecao := sessao.DB("empresa").C("cliente")
-
-
   //Extrair o texto da form submetida pelo usuario
   r.ParseForm()
 	nome := r.PostFormValue("busca")
@@ -54,8 +52,37 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, clientes)
 }
 
+func CadastroHandler(w http.ResponseWriter, r *http.Request) {
+  //inicia uma sessão no mongodb e trata um possivel erro
+  url := "localhost"
+  sessao, err := mgo.Dial(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+  //Abre a coleção cliente
+  colecao := sessao.DB("empresa").C("cliente")
+  cliente := Cliente{}
+  //Extrair os textos da form submetida pelo usuario
+  if r.Method == "POST" {
+    r.ParseForm()
+    cliente = Cliente{Nome: r.PostFormValue("nome"),
+            Email: r.PostFormValue("email"),
+            Nasc: r.PostFormValue("nasc"),
+            Doc: r.PostFormValue("doc"),
+            Tipo: r.PostFormValue("tipo"),
+            End: r.PostFormValue("end")}
+    //Inserir cliente no banco de dados
+    colecao.Insert(cliente)
+  }
+  sessao.Close()  //encerrar sessão
+  //Abrimos nosso template da pagina inicial
+  t, _ := template.ParseFiles("templates/cadastro.html")
+	t.Execute(w, cliente)
+}
+
 func main() {
   fmt.Println("Listening at http://localhost:8082/")
 	http.HandleFunc("/", IndexHandler)
+  http.HandleFunc("/cadastro", CadastroHandler)
 	http.ListenAndServe(":8082", nil) //Servidor
 }
